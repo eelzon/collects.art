4de55827721811f125e1c354faeb684e39f0abec
Nozlee Samadzadeh <nozlee.samadzadeh@voxmedia.com>
2017-04-18 07:52:39 -0400

    add start of mustache templates for rendering collects

--- site/functions/index.js
+++ site/functions/index.js
@@ -1,8 +1,25 @@
 var functions = require('firebase-functions');
+var cors = require('cors')({ origin: true });
+var Mustache = require('mustache');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
+exports.template = functions.https.onRequest((req, res) => {
+  cors(req, res, () => {
+    var collect = JSON.parse(req.body);
+
+    var view = {
+      title: collect.title,
+      calc: function () {
+        return 2 + 4;
+      }
+    };
+
+    var template = Mustache.render(`
+      <h1>{{title}}</h1>
+      <!-- template data -->
+    `, view);
+
+    res.send(template);
+  });
+});
