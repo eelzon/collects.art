819e6390edbd64df413736048a107f3efe0c3bbc
Nozlee Samadzadeh <nozlee.samadzadeh@voxmedia.com>
2017-04-20 17:56:37 -0400

    the first template!

--- site/functions/index.js
+++ site/functions/index.js
@@ -1,33 +1,61 @@
 var functions = require('firebase-functions');
 var cors = require('cors')({ origin: true });
-var Mustache = require('mustache');
+var Handlebars = require('handlebars');
 
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
 exports.template = functions.https.onRequest((req, res) => {
   cors(req, res, () => {
     var collect = JSON.parse(req.body);
-
-    var view = {
-      title: collect.title,
-      calc: function () {
-        return 2 + 4;
-      }
-    };
-
-    var template = Mustache.render(`
-      <h1>{{title}}</h1>
-      <!-- template data -->
-    `, view);
-
-    Object.keys(collect.entries || {}).forEach((timestamp) => {
-      var entry = collect.entries[timestamp];
-      template = template + `
-        <h4>${entry.title}</h4>
-        <div><img src="${entry.image}" /></div>
-      `;
-    });
-
-    res.send(template);
+    res.send(template1(collect));
   });
 });
+
+function template1(collect) {
+  var html = `
+    <style type="text/css">
+      h1 {
+        text-align:center;
+      }
+      body {
+          background-image: url("http://meryn.ru/rhizome/harlequin.png");
+      }
+    </style>
+    <h1>{{collect.title}}</h1>
+    <hr>
+    {{#each collect.entries}}
+      {{#ifThird @index}}
+        <p style="width: 50%; text-align: center; margin: 0 auto;">{{this.title}}</p>
+        <hr>
+      {{else}}
+        {{#ifSecond @index}}
+          <p style="width: 50%; text-align: left; margin-right: auto;">{{this.title}}</p>
+          <hr>
+        {{else}}
+          <p style="width: 50%; text-align: right; margin-left: auto;">{{this.title}}</p>
+          <hr>
+        {{/ifSecond}}
+      {{/ifThird}}
+    {{/each}}
+  `;
+
+  var template = Handlebars.compile(html);
+
+  Handlebars.registerHelper('ifThird', function (index, options) {
+    if ((index + 1) % 3 == 0){
+      return options.fn(this);
+    } else {
+      return options.inverse(this);
+    }
+  });
+
+  Handlebars.registerHelper('ifSecond', function (index, options) {
+    if ((index + 1) % 2 == 0){
+      return options.fn(this);
+    } else {
+      return options.inverse(this);
+    }
+  });
+
+  return template({ collect });
+}
