fa8a4103c20d4826dddb0c1213b01ca19c5940b9
Nozlee Samadzadeh <nozlee.samadzadeh@voxmedia.com>
2017-04-21 01:33:07 -0400

    template tweaks

--- site/functions/index.js
+++ site/functions/index.js
@@ -11,26 +11,26 @@ exports.template = functions.https.onRequest((req, res) => {
     switch(template) {
     case 1:
       res.send(template1(collect));
+    // case 2:
+    //   res.send(template2(collect));
     case 2:
-      res.send(template2(collect));
-    case 3:
       res.send(template3(collect));
-    case 4:
+    case 3:
       res.send(template4(collect));
-    case 5:
+    case 4:
       res.send(template5(collect));
-    case 6:
+    case 5:
       res.send(template6(collect));
-    case 7:
+    case 6:
       res.send(template7(collect));
-    case 8:
+    case 7:
       res.send(template8(collect));
-    case 9:
-      res.send(template9(collect));
-    case 10:
-      res.send(template10(collect));
-    case 11:
-      res.send(template11(collect));
+    // case 9:
+    //   res.send(template9(collect));
+    // case 10:
+    //   res.send(template10(collect));
+    // case 11:
+    //   res.send(template11(collect));
     }
   });
 });
@@ -104,15 +104,15 @@ function template1(collect) {
   return template({ title: collect.title, entries: links });
 }
 
-function template2(collect) {
-  var html = `
+// function template2(collect) {
+//   var html = `
 
-  `;
+//   `;
 
-  var template = Handlebars.compile(html);
+//   var template = Handlebars.compile(html);
 
-  return template({});
-}
+//   return template({});
+// }
 
 function template3(collect) {
   var html = `
@@ -128,7 +128,7 @@ function template3(collect) {
         margin-top: 40px;
       }
       .description {
-        width: 460px;
+        max-width: 460px;
         margin: 40px auto;
         background-color: rgba(150,135,165,1);
         color: white;
@@ -175,6 +175,7 @@ function template4(collect) {
       }
       .column-group {
         display: inline-block;
+        width: 100%;
       }
       .column {
         width: 31%;
@@ -182,7 +183,7 @@ function template4(collect) {
         margin: 0 1%;
       }
       .column img {
-        width: 100%;
+        max-width: 100%;
       }
     </style>
     <h1>{{title}}</h1>
@@ -199,7 +200,6 @@ function template4(collect) {
 
   for (var i = 0; i < keys.length; i += 3) {
     var group = '';
-    // Do your work with array[i], array[i+1]...array[i+N-1]
     [i, i + 1, i + 2].forEach((index) => {
       if (index >= keys.length) {
         return;
@@ -266,7 +266,6 @@ function template5(collect) {
   for (var i = 0; i < keys.length; i += 6) {
     var headers = '';
     var images = '';
-    // Do your work with array[i], array[i+1]...array[i+N-1]
     [i, i + 1, i + 2, i + 3, i + 4, i + 5].forEach((index) => {
       if (index >= keys.length) {
         return;
@@ -274,12 +273,8 @@ function template5(collect) {
       var entry = entries[keys[index]];
       var image = entry.image ? `<img src="${entry.image}" />` : '';
       var title = entry.title || '';
-      if (title) {
-        headers = headers + `<th>${title}</th>`;
-      }
-      if (image) {
-        images = images + `<td>${image}</td>`;
-      }
+      headers = headers + `<th>${title}</th>`;
+      images = images + `<td>${image}</td>`;
     });
     if (headers) {
       rows = rows + `<tr>${headers}</tr>`;
@@ -294,12 +289,65 @@ function template5(collect) {
 
 function template6(collect) {
   var html = `
-
+    <style type="text/css">
+      h1 {
+        text-align: center;
+      }
+      body {
+        font-family: 'Times New Roman', Times, serif;
+        margin: 8px;
+        background-image: url("http://meryn.ru/rhizome/tulip-bg-light.gif");
+      }
+      .description {
+       color: #9B979F;
+       background-color: thistle;
+      }
+      a {
+        text-decoration: none;
+        color: inherit;
+      }
+      img {
+        margin-bottom: 8px;
+        max-width: 550px;
+        max-height: 375px;
+      }
+    </style>
+    <h1>{{title}}</h1>
+    <hr>
+    <p class="description">
+      {{#if random.image}}
+        <a href="{{random.image}}">
+      {{/if}}
+      {{random.title}}
+      {{#if random.image}}
+        </a>
+      {{/if}}
+    </p>
+    <hr>
+    {{#each rest}}
+      {{#if this.image}}
+        <p><img src="{{this.image}}" title="{{this.title}}" alt="{{this.title}}" /></p>
+      {{/if}}
+    {{/each}}
+    <hr>
   `;
 
   var template = Handlebars.compile(html);
 
-  return template({});
+  var keys = Object.keys(collect.entries || {});
+  var index = Math.floor(Math.random() * keys.length);
+
+  var rest = [];
+  var random;
+  keys.forEach((key, i) => {
+    if (i !== index) {
+      rest.push(collect.entries[key]);
+    } else {
+      random = collect.entries[key];
+    }
+  });
+
+  return template({ title: collect.title, random: random, rest: rest });
 }
 
 function template7(collect) {
@@ -321,8 +369,8 @@ function template7(collect) {
     <h1>{{title}}</h1>
     <a href="/">back</a>
     <hr>
-    {{#if image}}
-      <img src="{{image}}">
+    {{#if entry.image}}
+      <img src="{{entry.image}}" title="{{entry.title}}" alt="{{entry.title}}">
     {{/if}}
     <hr>
     <a href="/">back</a>
@@ -333,5 +381,205 @@ function template7(collect) {
   var keys = Object.keys(collect.entries || {});
   var index = Math.floor(Math.random() * keys.length);
 
-  return template({ title: collect.title, image: collect.entries[keys[index]].image });
+  return template({ title: collect.title, entry: collect.entries[keys[index]] });
 }
+
+function template8(collect) {
+  var html = `
+    <style type="text/css">
+      h1 {
+        text-align: center;
+      }
+      body {
+        font-family: 'Times New Roman', Times, serif;
+        margin: 8px;
+        background-image: url("http://meryn.ru/rhizome/ty-bg-light.png");
+      }
+      .poem {
+        background: white;
+        color: dimgrey;
+        max-width: 600px;
+        padding: 10px;
+        margin-right:auto
+      }
+      img {
+        max-width: 150px;
+        max-height: 150px;
+      }
+    </style>
+    <h1>{{title}}</h1>
+    <hr>
+
+    {{#each entries}}
+      {{#if this.image}}
+        {{#if this.title}}
+          <a href="#{{@index}}">
+        {{/if}}
+          <img src="{{this.image}}" />
+        {{#if this.title}}
+          </a>
+        {{/if}}
+      {{/if}}
+    {{/each}}
+
+    {{#each entries}}
+      {{#if this.title}}
+        <a name="{{@index}}"></a>
+        <p class="poem">
+          {{this.title}}
+        </p>
+      {{/if}}
+    {{/each}}
+  `;
+
+  var template = Handlebars.compile(html);
+
+  return template({ title: collect.title, entries: collect.entries });
+}
+
+// function template9(collect) {
+//   var html = `
+
+//   `;
+
+//   var template = Handlebars.compile(html);
+
+//   return template({});
+// }
+
+// function template10(collect) {
+//   var html = `
+//     <style type="text/css">
+//       h1 {
+//         text-align: center;
+//         text-transform: uppercase;
+//       }
+//       body {
+//         font-family: 'Times New Roman', Times, serif;
+//         margin: 8px;
+//         background-image: url("http://meryn.ru/rhizome/rain-bg-lighter.png");
+//       }
+//       .content {
+//         width: 800px;
+//         margin: 20px auto;
+//         text-align: left;
+//       }
+//       .column-group {
+//         display: inline-block;
+//       }
+//       .column {
+//         width: 230px;
+//         float: left;
+//         margin: 0 30px 0 0;
+//       }
+//       .column.last {
+//         margin: 0;
+//       }
+//       .entry {
+//         padding: 4px 8px;
+//         background-color: rgba(164,172,185, .8);
+//       }
+//     </style>
+//     <h1>{{title}}</h1>
+//     <div class="content">
+//       {{#each entries}}
+//         {{{this}}}
+//       {{/each}}
+//     </div>
+//   `;
+
+//   // <p></p>
+//   // <div class="column-group">
+//   //   <p class="column"></p>
+//   //   <p class="column"></p>
+//   //   <p class="column last"></p>
+//   // </div>
+//   // <p></p>
+//   // <p></p>
+//   var entries = collect.entries;
+//   var keys = Object.keys(entries || {});
+//   var content = '';
+
+//   var formatEntry = (entry, customClass) => {
+//     var image = entry.image ? `<img src="${entry.image}" />` : '';
+//     var title = entry.title ? `<p>${entry.title}</p>` : '';
+//     if (image || title) {
+//       return `<div class="entry ${customClass}">${image}${title}</div>`
+//     }
+//     return '';
+//   };
+
+//   for (var i = 0; i < keys.length; i += 6) {
+//     var group = '';
+//     if (i >= keys.length) {
+//       return;
+//     } else {
+//       group = group + formatEntry(entries[keys[i]], '');
+//     }
+//     [i + 1, i + 2, i + 3].forEach((index) => {
+//       if (index >= keys.length) {
+//         return;
+//       }
+//       var entry = entries[keys[index]];
+//       group = group + formatEntry(entry, 'column');
+//       if (group) {
+//         group = group + `<div class="column">${image}${title}</div>`;
+//       }
+//     });
+//     if (group) {
+//       content = content + `<div class="column-group">${group}</div>`;
+//     }
+//   }
+
+//   var template = Handlebars.compile(html);
+
+//   return template({});
+// }
+
+// function template11(collect) {
+//   var html = `
+//     <style type="text/css">
+//       body {
+//         text-align: center;
+//         background-image: url("http://meryn.ru/rhizome/princessrose-bg-light.gif");
+//       }
+//     </style>
+//     <h3><em>{{title}}</em></h3>
+
+//     <p>2</p>
+//     <p>4</p>
+//     <p>7</p>
+//     <p>8</p>
+//     <p>11</p>
+//     <p>9</p>
+//     <p>11</p>
+//     <p>8</p>
+//     <p>10</p>
+//     <p>5</p>
+//     <p>3</p>
+//     <p>1</p>
+
+//     <p><a href="/">HOME</a></p>
+//   `;
+
+//   var template = Handlebars.compile(html);
+
+//   var entries = collect.entries;
+//   var keys = Object.keys(entries || {});
+
+//   var links = keys.map((key) => {
+//     var entry = entries[key];
+//     if (entry.title && entry.image) {
+//       return `<a href="${entry.image}">${entry.title}</a>`;
+//     } else if (entry.title) {
+//       return entry.title;
+//     } else if (entry.image) {
+//       return `<a href="${entry.image}">untitled</a>`;
+//     }
+//     return '';
+//   });
+
+//   var rows = [2, 4, 7, 8, 11, 9, 11, 8, 10, 5, 3, 1];
+
+//   return template({});
+// }
